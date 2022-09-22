602fd8938ee74a08cb13a1e1820a6ca4b578049b
Picadio <56791168+Picadio@users.noreply.github.com>
2022-09-22 20:14:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,28 +102,7 @@ def in_2(x):
 	return q
 
 
-
-class twomodal(Modal, title="Вирішити тест"):
-	async def on_submit(self, interaction: discord.Interaction):
-		global ans2
-		global rand2
-		global time2
-		if(ans2.value == in_2(rand2)):
-			em = discord.Embed(colour= 2883328, title=f"Переведення в двійкову систему числа {rand2}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans2.value)
-			em.add_field(name="Правильна відповідь:", value=in_2(rand2))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time2, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
-			await interaction.response.send_message(embed=em, ephemeral=True)
-		else:
-			em = discord.Embed(colour= 16711680, title=f"Переведення в двійкову систему числа {rand2}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans2.value)
-			em.add_field(name="Правильна відповідь:", value=in_2(rand2))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time2, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
-			await interaction.response.send_message(embed=em, ephemeral=True)
+	
 
 def in_8(x):
 	list = []
@@ -137,27 +116,8 @@ def in_8(x):
 		q+=s
 	return q
 
-class eightmodal(Modal, title="Вирішити тест"):
-	async def on_submit(self, interaction: discord.Interaction):
-		global ans8
-		global rand8
-		global time8
-		if(ans8.value == in_8(rand8)):
-			em = discord.Embed(colour= 2883328, title=f"Переведення в вісімкову систему числа {rand8}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans8.value)
-			em.add_field(name="Правильна відповідь:", value=in_8(rand8))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time8, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
-			await interaction.response.send_message(embed=em, ephemeral=True)
-		else:
-			em = discord.Embed(colour= 16711680, title=f"Переведення в вісімкову систему числа {rand8}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans8.value)
-			em.add_field(name="Правильна відповідь:", value=in_8(rand8))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time8, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
-			await interaction.response.send_message(embed=em, ephemeral=True)
+
+	
 		
 
 def in_16(x):
@@ -186,27 +146,7 @@ def in_16(x):
 		q+=s
 	return q
 
-class sixteenmodal(Modal, title="Вирішити тест"):
-	async def on_submit(self, interaction: discord.Interaction):
-		global ans16
-		global rand16
-		global time16
-		if(ans16.value.upper() == in_16(rand16)):
-			em = discord.Embed(colour= 2883328, title=f"Переведення в шістнадцяткову систему числа {rand16}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans16.value)
-			em.add_field(name="Правильна відповідь:", value=in_16(rand16))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time16, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
-			await interaction.response.send_message(embed=em, ephemeral=True)
-		else:
-			em = discord.Embed(colour= 16711680, title=f"Переведення в шістнадцяткову систему числа {rand16}", description="=======================================")
-			em.add_field(name="Відповідь учасника:", value=ans16.value)
-			em.add_field(name="Правильна відповідь:", value=in_16(rand16))
-			em.add_field(name="Часу було витрачено", value=interaction.created_at-time16, inline=False)
-			em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
-			em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
-			await interaction.response.send_message(embed=em, ephemeral=True)
+	
 
 class PersistentViewtest(discord.ui.View):
 	def __init__(self):
@@ -214,41 +154,87 @@ class PersistentViewtest(discord.ui.View):
 
 	@discord.ui.button(custom_id='persistent_view:test2', label='2', style=discord.ButtonStyle.blurple)
 	async def test2(self, interaction: discord.Interaction, button: discord.ui.Button):
-			global rand2
+
 			rand2 = random.randint(1, 999)
-			modal = twomodal()
-			global ans2
-			global time2
+			modal = Modal(title="Вирішити тест")
+
 			time2 = interaction.created_at
 			ans2 = TextInput(label=f'Переведіть число {rand2} в 2 систему числення', required=True)
 			modal.add_item(ans2)
-
+			async def modal_submit(interaction):
+				if(ans2.value == in_2(rand2)):
+					em = discord.Embed(colour= 2883328, title=f"Переведення в двійкову систему числа {rand2}", description="=======================================")
+					em.add_field(name="Відповідь учасника:", value=ans2.value)
+					em.add_field(name="Правильна відповідь:", value=in_2(rand2))
+					em.add_field(name="Часу було витрачено", value=interaction.created_at-time2, inline=False)
+					em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+					em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
+					await interaction.response.send_message(embed=em, ephemeral=True)
+				else:
+					em = discord.Embed(colour= 16711680, title=f"Переведення в двійкову систему числа {rand2}", description="=======================================")
+					em.add_field(name="Відповідь учасника:", value=ans2.value)
+					em.add_field(name="Правильна відповідь:", value=in_2(rand2))
+					em.add_field(name="Часу було витрачено", value=interaction.created_at-time2, inline=False)
+					em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+					em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
+					await interaction.response.send_message(embed=em, ephemeral=True)
+			modal.on_submit = modal_submit
 			await interaction.response.send_modal(modal)
 			print(in_2(rand2))
 	@discord.ui.button(custom_id='persistent_view:test8', label='8', style=discord.ButtonStyle.blurple)
 	async def test8(self, interaction: discord.Interaction, button: discord.ui.Button):
-		global rand8
 		rand8 = random.randint(1, 999)
-		modal = eightmodal()
-		global ans8
-		global time8
+		modal = Modal(title="Вирішити тест")
 		time8 = interaction.created_at
 		ans8 = TextInput(label=f'Переведіть число {rand8} в 8 систему числення', required=True)
-		modal.add_item(ans8)
-
+		modal.add_item(ans8)		
+		async def modal_submit(interaction):
+			if(ans8.value == in_8(rand8)):
+				em = discord.Embed(colour= 2883328, title=f"Переведення в вісімкову систему числа {rand8}", description="=======================================")
+				em.add_field(name="Відповідь учасника:", value=ans8.value)
+				em.add_field(name="Правильна відповідь:", value=in_8(rand8))
+				em.add_field(name="Часу було витрачено", value=interaction.created_at-time8, inline=False)
+				em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+				em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
+				await interaction.response.send_message(embed=em, ephemeral=True)
+			else:
+				em = discord.Embed(colour= 16711680, title=f"Переведення в вісімкову систему числа {rand8}", description="=======================================")
+				em.add_field(name="Відповідь учасника:", value=ans8.value)
+				em.add_field(name="Правильна відповідь:", value=in_8(rand8))
+				em.add_field(name="Часу було витрачено", value=interaction.created_at-time8, inline=False)
+				em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+				em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
+				await interaction.response.send_message(embed=em, ephemeral=True)
+		modal.on_submit = modal_submit
 		await interaction.response.send_modal(modal)
 		print(in_8(rand8))
 	@discord.ui.button(custom_id='persistent_view:test16', label='16', style=discord.ButtonStyle.blurple)
 	async def test16(self, interaction: discord.Interaction, button: discord.ui.Button):
-		global rand16
+
 		rand16 = random.randint(1, 999)
-		modal = sixteenmodal()
-		global ans16
-		global time16
+		modal = Modal(title="Вирішити тест")
 		time16 = interaction.created_at
 		ans16 = TextInput(label=f'Переведіть число {rand16} в 16 систему числення', required=True)
 		modal.add_item(ans16)
-
+		async def modal_submit(interaction):
+			if(ans16.value.upper() == in_16(rand16)):
+				em = discord.Embed(colour= 2883328, title=f"Переведення в шістнадцяткову систему числа {rand16}", description="=======================================")
+				em.add_field(name="Відповідь учасника:", value=ans16.value)
+				em.add_field(name="Правильна відповідь:", value=in_16(rand16))
+				em.add_field(name="Часу було витрачено", value=interaction.created_at-time16, inline=False)
+				em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+				em.set_author(name="OK", icon_url="https://cdn.pixabay.com/photo/2017/03/28/01/46/check-mark-2180770_960_720.png")
+				await interaction.response.send_message(embed=em, ephemeral=True)
+			else:
+				em = discord.Embed(colour= 16711680, title=f"Переведення в шістнадцяткову систему числа {rand16}", description="=======================================")
+				em.add_field(name="Відповідь учасника:", value=ans16.value)
+				em.add_field(name="Правильна відповідь:", value=in_16(rand16))
+				em.add_field(name="Часу було витрачено", value=interaction.created_at-time16, inline=False)
+				em.set_footer(text=f"Вирішував:{interaction.user.name}", icon_url=interaction.user.display_avatar)
+				em.set_author(name="Bad", icon_url="https://img1.freepng.ru/20180605/yop/kisspng-computer-icons-true-false-quiz-world-social-med-5b162251e0d412.6314358315281772339209.jpg")
+				await interaction.response.send_message(embed=em, ephemeral=True)
+				
+		modal.on_submit = modal_submit
 		await interaction.response.send_modal(modal)
 		print(in_16(rand16))
 
