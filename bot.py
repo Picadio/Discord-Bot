b043d4692e833d5c25065ab9efc51a3132a76748
Alex <56791168+Picadio@users.noreply.github.com>
2022-09-25 10:47:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -342,7 +342,7 @@ async def hi(ctx, message):
 
 @Bot.command(pass_context=True)
 async def bye(ctx, message):
-    await ctx.message.reply("F")
+    await ctx.message.reply("LOL")
 
 
 token = os.environ.get("BOT_TOKEN")
