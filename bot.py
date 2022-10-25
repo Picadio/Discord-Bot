52806f752d199c964f5709691cbce910f6dc49f3
Picadio <sashabedryk2004@gmail.com>
2022-10-25 15:34:43 +0300

    BIRTHDAY

--- bot.py
+++ bot.py
@@ -73,13 +73,14 @@ async def check_birthday_all(ctx):
     row = cursor.fetchone()
     embed = discord.Embed(title="🎂 Дні народження 🎂", description="============================")
     embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1719/1719458.png")
+    embed.set_footer(text="Викликано {0}".format(ctx.message.author.mention))
     while row is not None:
         user = Bot.get_user(int(row[0]))
         md = str(row[1])
         if len(md) == 3:
             md = "0" + md
 
-        embed.add_field(name="{0}".format(user.mention), value=md[0] + md[1] + "." + md[2] + md[3] + "." + str(row[2]), inline=True)
+        embed.add_field(name=user.name, value=md[0] + md[1] + "." + md[2] + md[3] + "." + str(row[2]), inline=True)
         row = cursor.fetchone()
     await ctx.reply(embed=embed)
 
