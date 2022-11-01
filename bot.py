ad371b8a059e667cb8f0caf18a68b6395cd384c0
Picadio <sashabedryk2004@gmail.com>
2022-11-01 15:39:27 +0200

    BIRTHDAY

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
-import datetime
 from discord.ext import tasks
-
-import discord
 from Class import *
 import os
 from discord import app_commands
 import psycopg2
+import datetime
+import pytz
+from itertools import cycle
 
+# ====================== Constant variable ======================
+
+UA_timezone = pytz.timezone("Europe/Kiev")
+utc = (int(datetime.datetime.now(UA_timezone).strftime("%H"))-int(datetime.datetime.now().strftime("%H"))+24) % 24
+status = cycle(["як написати бота", "як приготувати хом'яка"])
 
 db_url = str(os.environ.get("DATABASE_URL"))
 db_url = db_url.replace("postgres://", "")
@@ -18,30 +23,7 @@ db_name = x[2].split("/")[1]
 
 Bot = bot()
 
-
-
-#    COMMAND
-
-
-@Bot.command(pass_context=True)
-async def prepare(ctx, message):
-    if ctx.message.author.id == 343279631807545356:
-        if message == "0":
-            await ctx.channel.send(
-                embed=discord.Embed(colour=0x39d0d6, title="Room management panel",
-                                    description="Name - change room name \n\n Lock - lock room \n\n Unlock - unlock "
-                                                "room \n\n SCP - Set count people"),
-                view=ControlPanel()
-            )
-            await ctx.message.delete()
-        if message == "1":
-            await ctx.channel.send(
-                embed=discord.Embed(colour=0x39d0d6, title="Тренувальник для переводу чисел в різні системи числення",
-                                    description="Оберіть в яку систему числення ви хочете переводити число"),
-                view=Tester()
-            )
-            await ctx.message.delete()
-
+# ====================== Members Command ======================
 
 @Bot.command(pass_context=True)
 async def determinant(ctx, size, *, matrix):
@@ -75,11 +57,10 @@ async def check_birthday_all(ctx):
     embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1719/1719458.png")
     embed.set_footer(text="Викликано: {0}".format(ctx.message.author.display_name), icon_url=ctx.message.author.avatar)
     while row is not None:
-        user = ctx.message.guild.get_member(int(row[0]))
+        user = Bot.get_guild(1020640631175004160).get_member(int(row[0]))
         md = str(row[1])
         if len(md) == 3:
             md = "0" + md
-
         embed.add_field(name=user.display_name, value=md[0] + md[1] + "." + md[2] + md[3] + "." + str(row[2]), inline=True)
         row = cursor.fetchone()
     await ctx.reply(embed=embed)
@@ -89,28 +70,56 @@ async def check_birthday_all(ctx):
 @app_commands.guilds(discord.Object(id="1020640631175004160"))
 async def setbirthday(ctx, day, month, year):
     if len(day) == 1:
-        day = "0"+day
+        day = "0" + day
     if len(month) == 1:
-        month = "0"+month
+        month = "0" + month
 
     table = psycopg2.connect(dbname=db_name, user=db_user,
                              password=db_password, host=db_host)
     cursor = table.cursor()
-    cursor.execute('''SELECT * FROM birthday_tab where id = {0}'''.format(str(ctx.message.author.id)))
+    cursor.execute('''SELECT * FROM birthday_tab where id = {0}'''.format(
+        str(ctx.message.author.id)))
     row = cursor.fetchone()
     if row is not None:
-        cursor.execute('''UPDATE birthday_tab set month_day={0}, yr={1} where id={2}'''.format(day+month, int(year), str(ctx.message.author.id)))
+        cursor.execute('''UPDATE birthday_tab set month_day={0}, yr={1} where id={2}'''.format(
+            day + month, int(year), str(ctx.message.author.id)))
     else:
-        cursor.execute('''INSERT INTO birthday_tab (id, month_day, yr) VALUES ({0}, {1}, {2})'''.format(str(ctx.message.author.id), day+month, int(year)))
+        cursor.execute('''INSERT INTO birthday_tab (id, month_day, yr) VALUES ({0}, {1}, {2})'''.format(
+            str(ctx.message.author.id), day + month, int(year)))
+
     table.commit()
     cursor.close()
     table.close()
     embed = discord.Embed(title="✅ Успішно", color=0x2bff00)
-    embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/2362/2362432.png")
-    embed.add_field(name="Дата народження", value=day+"."+month+"."+year, inline=True)
+    embed.set_thumbnail(
+        url="https://cdn-icons-png.flaticon.com/512/2362/2362432.png")
+    embed.add_field(name="Дата народження", value=day + "." + month + "." + year, inline=True)
     embed.set_footer(text="Встановлено для {0}".format(ctx.message.author.name))
     await ctx.reply(embed=embed)
 
+
+# ====================== Developers Commands ======================
+
+@Bot.command(pass_context=True)
+async def prepare(ctx, message):
+    if ctx.message.author.id == 343279631807545356:
+        if message == "0":
+            await ctx.channel.send(
+                embed=discord.Embed(colour=0x39d0d6, title="Room management panel",
+                                    description="Name - change room name \n\n Lock - lock room \n\n Unlock - unlock "
+                                                "room \n\n SCP - Set count people"),
+                view=ControlPanel()
+            )
+            await ctx.message.delete()
+        if message == "1":
+            await ctx.channel.send(
+                embed=discord.Embed(colour=0x39d0d6, title="Тренувальник для переводу чисел в різні системи числення",
+                                    description="Оберіть в яку систему числення ви хочете переводити число"),
+                view=Tester()
+            )
+            await ctx.message.delete()
+    else:
+        await ctx.reply("Ця команда доступна тільки розробнику")
 
 
 @Bot.command(pass_context=True)
@@ -134,27 +143,32 @@ async def crtable(ctx):
 async def setbirthday_for(ctx, mem: discord.Member, day, month, year):
     if str(ctx.message.author.id) == "343279631807545356":
         if len(day) == 1:
-            day = "0"+day
+            day = "0" + day
         if len(month) == 1:
-            month = "0"+month
+            month = "0" + month
         if year == "None":
             year = "0"
 
         table = psycopg2.connect(dbname=db_name, user=db_user,
                                  password=db_password, host=db_host)
         cursor = table.cursor()
-        cursor.execute('''SELECT * FROM birthday_tab where id = {0}'''.format(str(mem.id)))
+        cursor.execute('''SELECT * FROM birthday_tab where id = {0}'''.format(
+            str(mem.id)))
         row = cursor.fetchone()
         if row is not None:
-            cursor.execute('''UPDATE birthday_tab set month_day={0}, yr={1} where id={2}'''.format(day+month, int(year), str(mem.id)))
+            cursor.execute(
+                '''UPDATE birthday_tab set month_day={0}, yr={1} where id={2}'''.
+                format(day + month, int(year), str(mem.id)))
         else:
-            cursor.execute('''INSERT INTO birthday_tab (id, month_day, yr) VALUES ({0}, {1}, {2})'''.format(str(mem.id), day+month, int(year)))
+            cursor.execute(
+                '''INSERT INTO birthday_tab (id, month_day, yr) VALUES ({0}, {1}, {2})'''
+                .format(str(mem.id), day + month, int(year)))
         table.commit()
         cursor.close()
         table.close()
         embed = discord.Embed(title="✅ Успішно", color=0x2bff00)
         embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/2362/2362432.png")
-        embed.add_field(name="Дата народження", value=day+"."+month+"."+year, inline=True)
+        embed.add_field(name="Дата народження", value=day + "." + month + "." + year, inline=True)
         embed.set_footer(text="Встановлено для {0}".format(mem.name))
         await ctx.reply(embed=embed)
         await ctx.message.delete()
@@ -162,7 +176,9 @@ async def setbirthday_for(ctx, mem: discord.Member, day, month, year):
         await ctx.send("Ця команда доступна тільки розробнику")
 
 
-@tasks.loop(time = datetime.time(21, 0, 0))
+# ====================== Auto Commands ======================
+
+@tasks.loop(time=datetime.time(22-utc+2, 0, 0))
 async def happy_birthday():
     table = psycopg2.connect(dbname=db_name,
                              user=db_user,
@@ -170,27 +186,19 @@ async def happy_birthday():
                              host=db_host)
     cursor = table.cursor()
     cursor.execute('''SELECT * FROM birthday_tab where month_day={0}'''.format(
-        int(datetime.datetime.now().strftime("%d%m"))))
+        int(datetime.datetime.now(UA_timezone).strftime("%d%m"))))
     row = cursor.fetchone()
     channel = Bot.get_guild(1020640631175004160).get_channel(1033134557467267135)
-    print(channel.name)
     while row is not None:
         user = Bot.get_guild(1020640631175004160).get_member(int(row[0]))
         if row[2] == "0":
-            embed = discord.Embed(
-                title="Member birthday",
-                color=0xff00bb,
-                description=
+            embed = discord.Embed(title="Member birthday", color=0xff00bb, description=
                 "=============================== \n З ДНЕМ НАРОДЖЕННЯ {0}! 🎂 \n =============================== \n Рік народження не вказаний"
                 .format(user.mention))
         else:
-            embed = discord.Embed(
-                title="Member birthday",
-                color=0xff00bb,
-                description=
+            embed = discord.Embed(title="Member birthday", color=0xff00bb, description=
                 "=============================== \n З ДНЕМ НАРОДЖЕННЯ {0}! 🎂 \n =============================== \n Виповнилося {1}"
-                .format(user.mention,
-                        datetime.datetime.now().year - int(row[2])))
+                .format(user.mention, datetime.datetime.now().year - int(row[2])))
         embed.set_author(name=user.name, icon_url=user.avatar)
         embed.set_thumbnail(url="https://i.imgur.com/wlA4lOm.gif")
         #embed.set_footer(text="Років виповнилося {0}".format(datetime.datetime.now().year - int(row[2])))
@@ -203,14 +211,21 @@ async def happy_birthday():
         row = cursor.fetchone()
     cursor.close()
     table.close()
-
+    picadio = Bot.get_user(343279631807545356)
+    await picadio.send("Happy checked")
     print("check birthday")
+
+
+@tasks.loop(seconds=60)
+async def change_status():
+    await Bot.change_presence(activity=discord.Activity(name=next(status), type=discord.ActivityType.watching))
 
 
 @Bot.event
 async def on_ready():
-    print("Bot is online")
     happy_birthday.start()
+    change_status.start()
+    print("Bot started at " + str(datetime.datetime.now(UA_timezone).strftime("%H:%M:%S %d.%m.%Y")))
     print(datetime.datetime.now())
 
 
